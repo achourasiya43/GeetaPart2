590d6c25c0f1d30bd0684bd4a72e490692e03574
rhythmsoft <achourasiya43@gmail.com>
2021-12-19 00:21:03 +0530

    CICD Proccess 123

---
+++ app/src/androidTest/java/com/gitapapp/love/MainActivityTest.java
@@ -0,0 +1,11 @@
+package com.gitapapp.love;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class MainActivityTest {
+    @Test
+    public void shouldDisplayExpenseButton() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
