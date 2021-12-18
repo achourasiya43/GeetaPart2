1325f8fe61f8cb0bfb552a2d6816ce7ab91bb6be
rhythmsoft <achourasiya43@gmail.com>
2021-12-19 01:29:10 +0530

    CICD Proccess instumention check3

--- app/src/androidTest/java/com/gitapapp/love/MainActivityTest.java
+++ app/src/androidTest/java/com/gitapapp/love/MainActivityTest.java
@@ -1,12 +1,10 @@
 package com.gitapapp.love;
 
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import org.junit.Rule;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class MainActivityTest {
-
 
     @Test
     public void shouldDisplayExpenseButton2() throws Exception {
